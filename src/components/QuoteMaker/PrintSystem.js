0c990341d8f92ede6bb2cb5e2cd0a35286e626a4
Jaragoro <jaragoro@hotmail.com>
2020-02-26 21:46:24 -0600

    Aplicación de Estilos al Componente PrintSystem
    Aún falta verificar el import de "Bootstrap" al proyecto para realmente ver los estilos.

--- src/components/QuoteMaker/PrintSystem.js
+++ src/components/QuoteMaker/PrintSystem.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
+import "../../pages/css/servicesCards.css";
 
 const PrintSystem = () => {
 
     return (
         <div>
-           <h1>Review Order PrintSystem</h1>
+           <div className="container-fluid card mb-3">
+               <div className="row no-gutters">
+                   <div className="col-md-4">
+                       <img src="" className="card-img" alt="Digital Print"/>
+                   </div>
+                   <div className="col-md-8">
+                       <div className="card-body">
+                           <h4>Digital</h4>
+                           <p>¡La más barata! Ideal para paquetes de 100 y 500 piezas.</p>
+                       </div>
+                   </div>
+               </div>
+           </div>
         </div>
     )
     }
