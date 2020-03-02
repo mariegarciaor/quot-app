353c6c8de7cc81bf3400161ae63daa5dd0c14b96
karly Gasavi <57203231+gasavika02@users.noreply.github.com>
2020-03-01 18:49:01 -0600

    Update LogIn.js
    pendientes: el link de
    olvidaste tu contraseña?
    y verificar los botones!

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -4,7 +4,36 @@ const LogIn = () => {
 
     return (
         <div>
-           <h1>Prueba Login</h1>
+           <h3>Inicia sesión para continuar </h3>
+           <img src="path/to/image" />
+
+
+           <div>
+               <i></i>
+               <h5>Correo electronico</h5>
+               <input type="email"/>
+               <h5>Contraseña</h5>
+               <input type="Password"/>
+               <i></i>
+ 
+               <link></link>
+               <button>inicia sesión</button>
+               <a class="btn btn-primary" href="/QuoteMarket" role="button">Link</a>
+
+
+               <link>¿Olvidaste tu contraseña?</link>
+
+
+               <input type ="checkbox"/><label>Recordar</label>
+
+               <p>¿No tienes cuenta?</p>
+               <Link to="/Register" className="">Registrate</Link>
+               
+
+           </div>
+
+
+           
         </div>
     )
     }
