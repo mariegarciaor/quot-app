5ab3cd8b8f478dab06addecc8d0ea35184c2805f
Jaragoro <jaragoro@hotmail.com>
2020-03-03 22:36:31 -0600

    Bootstrap, Sass, Css
    Aplicación de Bootstrap con Sass, a partir de archivos CSS en los estilos del archivo "PrintSystem". Favor de quitarlo de la parte de renderizado en el documento "index.js".

--- src/components/QuoteMaker/PrintSystem.js
+++ src/components/QuoteMaker/PrintSystem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import "../../pages/css/servicesCards.css";
+import "../../pages/css/main.css";
 
 const PrintSystem = () => {
 
     return (
-        <div>
-           <div className="container-fluid card mb-3">
+        <div className="container">
+           <div className="card mb-3">
                <div className="row no-gutters">
                    <div className="col-md-4">
                        <img src="" className="card-img" alt="Digital Print"/>
@@ -14,7 +14,35 @@ const PrintSystem = () => {
                    <div className="col-md-8">
                        <div className="card-body">
                            <h4>Digital</h4>
-                           <p>¡La más barata! Ideal para paquetes de 100 y 500 piezas.</p>
+                           <p>¡La más barata!<br />
+                           Ideal para paquetes de 100 y 500 piezas.</p>
+                       </div>
+                   </div>
+               </div>
+           </div>
+           <div className="card mb-3">
+               <div className="row no-gutters">
+                   <div className="col-md-4">
+                       <img src="" className="card-img" alt="Digital Print"/>
+                   </div>
+                   <div className="col-md-8">
+                       <div className="card-body">
+                           <h4>Offset</h4>
+                           <p>Perfecto para pedidos de 1000 piezas</p>
+                       </div>
+                   </div>
+               </div>
+           </div>
+           <div className="card mb-3">
+               <div className="row no-gutters">
+                   <div className="col-md-4">
+                       <img src="" className="card-img" alt="Digital Print"/>
+                   </div>
+                   <div className="col-md-8">
+                       <div className="card-body">
+                           <h4>Serigrafía</h4>
+                           <p>¡La más barata!<br />
+                           Ideal para paquetes de 100 y 500 piezas.</p>
                        </div>
                    </div>
                </div>
