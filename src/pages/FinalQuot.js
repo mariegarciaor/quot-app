ad2a2243d83e0ca573877c4780cbb9fc413c936c
mariegarciaor <57069316+mariegarciaor@users.noreply.github.com>
2020-03-15 14:38:00 -0600

    ReadMe + final-quot + PrintSystem
    ReadMe - Agregamos pasos a seguir para información de todos.
    
    FinalQuot - Agregamos página de FinalQuot para importar los resultados de todos los componentes del QuotMaker.
    
    PrintSystem - Convertimos los divs a buttons para poder empezar con handle click.

---
+++ src/pages/FinalQuot.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const FinalQuot = () => {
+
+    return (
+        <div>
+           <h1>Prueba Coming Soon</h1>
+        </div>
+    )
+    }
+
+
+export default FinalQuot;
--- src/pages/QuoteMaker.js
+++ src/pages/QuoteMaker.js
@@ -1,59 +1,18 @@
 import React from 'react';
 
+//Importar NavBar que se hará como un componente aparte
+
 const QuoteMaker = () => {
 
     return (
         <div>
-            <div id='section-header'>
-                <h4>PASO</h4>
-                <h1>Título de pregunta</h1>
-            </div>
-
-            <div id='card-option'>
-                <div id='card-container'>
-                    <div id='card-container-header'>
-                        <i className='' alt='icono'></i>
-                    </div>
-
-                    <div id='card-content'>
-                        <h2>Option name</h2>
-                        <p>Option description</p>
-                        <button>Seleccionar</button>
-                    </div>
-                </div>
-            </div>
-
-            <div id='card-option'>
-                <div id='card-container'>
-                    <div id='card-container-header'>
-                        <i></i>
-                    </div>
-
-                    <div id='card-content'>
-                        <h2>Option name</h2>
-                        <p>Option description</p>
-                        <button>Seleccionar</button>
-                    </div>
-                </div>
-            </div>
-
-            <div id='card-option'>
-                <div id='card-container'>
-                    <div id='card-container-header'>
-                        <i></i>
-                    </div>
-
-                    <div id='card-content'>
-                        <h2>Option name</h2>
-                        <p>Option description</p>
-                        <button>Seleccionar</button>
-                    </div>
-                </div>
-            </div>
+            <p>aquí van los componentes de QuoteMaker</p>
         </div>
         
     )
-    }
+}
+
+//Importar timeline (componente aparte) que se vaya actualizando de acuerdo al componente que esté en ese momento.
 
 
 export default QuoteMaker;
