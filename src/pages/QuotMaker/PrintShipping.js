a89006bd3ccab5951bc45975284b22fbbc30831d
mariegarciaor <57069316+mariegarciaor@users.noreply.github.com>
2020-05-07 22:03:56 -0500

    Se terminó el maquetado de las páginas del Quotamaker
    PrintDesign
    PrintFinish
    PrintPaper
    PrintShipping
    PrintUploadFile

--- src/pages/QuotMaker/PrintShipping.js
+++ src/pages/QuotMaker/PrintShipping.js
@@ -6,8 +6,8 @@ const PrintShipping = () => {
     return (
         <div class="container">
             <div>
-                <h3>PASO 3</h3>
-                <h1>¿En cuántas caras lo necesitas?</h1>
+                <h3>PASO 9</h3>
+                <h1>¿Qué tipo de envío te gustaría?</h1>
             </div>
             
             <div class="card mb-3">
@@ -18,8 +18,8 @@ const PrintShipping = () => {
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
-                            <h4 class="card-title">Una cara</h4>
-                            <p class="card-text">Diseño limpio, logotipo y datos en el frente de la tarjeta.</p>
+                            <h4 class="card-title">Envío regular</h4>
+                            <p class="card-text">Entre 4-5 días hábiles recibirás tu pedido.</p>
                         </div>
                     </div>
                 </div>
@@ -33,19 +33,33 @@ const PrintShipping = () => {
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
-                            <h4 class="card-title">Dos caras</h4>
-                            <p class="card-text">¡Doble impacto! Logotipo en frente y datos por detrás.</p>
+                            <h4 class="card-title">Envío exprés</h4>
+                            <p class="card-text">¡En 2 días hábiles están contigo!</p>
                         </div>
                     </div>
                 </div>
             </div>
 
-            {/* style="width: 25%;" */}
+            <div class="card mb-3">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
+                        <img src="https://www.beautycolorcode.com/84e1ff.png" class="card-img" alt="..."/>
+                        <Printer /> 
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h4 class="card-title">Sin envío</h4>
+                            <p class="card-text">Puedes recoger tu mismo tu pedido ;)</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* style="width: 90%;" */}
             <div class="progress">
-                <div class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">90%</div>
             </div>
         </div>
-    )
+        )
 }
 
 
