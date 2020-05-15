cbad74a1e2eb67bcd01676e49724d64dc1f6c4d5
Lorewii <57049900+Lorewii@users.noreply.github.com>
2020-05-14 21:08:03 -0500

    Cambios en orders

---
+++ src/pages/Orders/OrderCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// const OrderCard = (props) => {
+//   return (
+//     <div>
+//       <p>{props.title}</p>
+//       <p>{props.content}</p>
+//       <img src={props.image} alt="" />
+//     </div>
+//   );
+// };
+
+// export default OrderCard;
