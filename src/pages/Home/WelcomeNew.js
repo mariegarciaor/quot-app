d30f86a3b7ec8b1490e876f634c7f40d12bb1374
Francisco <frank@MacBook-Pro-de-Francisco.local>
2020-06-20 20:41:30 -0500

    CSS de welcomeNew

--- src/pages/Home/WelcomeNew.js
+++ src/pages/Home/WelcomeNew.js
@@ -5,18 +5,19 @@ import BigLogo from '../../styles/images/BigLogo.jpg';
 const WelcomeNew = () => {
 
     return (
-        <div class="container">
-            <div>
-                <h1>¡Bienvenido!</h1>
-                <img className="#" src={ BigLogo } alt="Logo-Quot" style={{width: 300, height: 300}}/>
-                <p>¿Te gustaría saber cuánto costarían tus tarjetas de presentación en tan solo 3 minutos?</p>
+        <div className="container-fluid wNew">
+            <div className="row wNew__Header">
+                <h1 className="col-12 Header__Title">¡Bienvenido!</h1>
+                <img className="Header__Image" src={ BigLogo } alt="Logo-Quot"/>
+                <p className="Header__Text">
+                    ¿Te gustaría saber cuánto costarían tus tarjetas de presentación en tan solo 3 minutos?
+                </p>
             </div>
-            <div>
-                <Link to="/Register" className="">Registrate</Link>
+            <div className="row wNew__Bottom">
+                <Link  to="/Register" className="col-12 col-sm-5 btn Bottom__Register">Registrate</Link>
+                <Link to="/Login" className="col-12 mt-2 mt-sm-0 btn offset-sm-2 col-sm-5 border-white text-white Bottom__Login">Inicia Sesión</Link>
                 <br/>
-                <Link to="/Login" className="">Inicia Sesión</Link>
-                <br/>
-                <Link to="/QuoteStart" className="">Continua como invitado</Link>
+                <Link to="/QuoteStart" className="col-12 text-center mt-3 text-white Bottom__Invited">Continua como invitado</Link>
             </div>
         </div>
     )
