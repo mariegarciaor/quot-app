5613879f7c4839becee1219b78d10fd5d86b3d73
Marie Garcia <57069316+mariegarciaor@users.noreply.github.com>
2020-07-16 08:33:24 -0500

    Update printsystem
    se agregan mejores imagenes temporales

--- src/pages/QuotMaker/PrintSystem.js
+++ src/pages/QuotMaker/PrintSystem.js
@@ -33,7 +33,7 @@ const PrintSystem = () => {
                 <div class="row row-cols-1 row-cols-md-3 quotemaker-form">
                     <div class="col mb-4">
                         <div class="quotemarkerCard card">
-                            <img src="https://cdn.thewirecutter.com/wp-content/uploads/2020/03/laser-printers-2020-lowres-9946-630x420.jpg" class="card-img-top" alt="..."/>
+                            <img src="https://i.pinimg.com/564x/25/a1/26/25a126dae6ccc394230c2e14d4ff99a0.jpg" class="card-img-top" alt="..."/>
                             <div class="card-body">
                                 <h5 class="card-title quotemaker-cardtitle">Card title</h5>
                                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -43,7 +43,7 @@ const PrintSystem = () => {
                     </div>
                     <div class="col mb-4">
                         <div class="card">
-                            <img src="https://dam.which.co.uk/6d22daf4-18cb-4348-9d30-29ce186d6da3.jpg" class="card-img-top" alt="..."/>
+                            <img src="https://i.pinimg.com/564x/2a/32/ff/2a32ffa4caa8e04dbd0680022720245d.jpg" class="card-img-top" alt="..."/>
                             <div class="card-body">
                                 <h5 class="card-title quotemaker-cardtitle">Card title</h5>
                                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -53,7 +53,7 @@ const PrintSystem = () => {
                     </div>
                     <div class="col mb-4">
                         <div class="card">
-                            <img src="https://www.rscsbd.com/image/cache/catalog/journal3/items/PRINTER/HP-500x500.jpg" class="card-img-top" alt="..."/>
+                            <img src="https://i.pinimg.com/564x/0b/20/3d/0b203da05883d6989c2583484bf84b67.jpg" class="card-img-top" alt="..."/>
                             <div class="card-body">
                                 <h5 class="card-title quotemaker-cardtitle">Card title</h5>
                                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
