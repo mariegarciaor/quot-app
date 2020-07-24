e359d5d8a44490c89df69738a6eba54e1d83d0a5
Marie Garcia <57069316+mariegarciaor@users.noreply.github.com>
2020-07-24 09:37:38 -0500

    Se hacen ajustes en login (flotante) y en WelcomeUser

--- src/pages/Home/WelcomeUser.js
+++ src/pages/Home/WelcomeUser.js
@@ -9,10 +9,11 @@ const WelcomeUser = () => {
             {/* <div className="welcome-cover col-8">
                 <img id="welcome-img" src="https://carddsgn.com/wp-content/uploads/2018/09/clevyr-business-cards-ar.gif" alt="Logo Quot"/>
             </div> */}
-            <div className="welcome-content col-xl-4 .col-lg-5 col-sm-8">
-                <img id="welcomeuser-img" src="https://aeealberta.org/wp-content/uploads/2018/10/profile.png"></img>
+            <div className="welcome-content col-xl-4 .col-lg-4 col-sm-8">
+                <img id="welcomeuser-img" src="https://diyshutters.co/wp-content/uploads/2019/06/r0bsDqL.png"></img>
                 <h3 className="title-1">¡Hola Usuario!</h3>
-                <p>¿Te gustaria hacer un pedido?</p>
+                <p>¿Te gustaria cotizar un pedido?</p>
+                <hr />
                 <Link to="/QuotMaker"><button className="btn buttom-cotizar">Cotiza ahora</button></Link>
                 <br />
                 <Link to="/MyOrders"><p className="link-1" id="revisar-pedidos">Revisar mis pedidos</p></Link>
