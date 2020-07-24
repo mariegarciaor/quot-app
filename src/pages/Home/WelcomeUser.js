a37d224afc1b3eb77325e84cbf04272dbb08727c
Marie Garcia <57069316+mariegarciaor@users.noreply.github.com>
2020-07-24 07:33:31 -0500

    Se termina WelcomeUser

--- src/pages/Home/WelcomeUser.js
+++ src/pages/Home/WelcomeUser.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import BigLogo from '../../styles/images/BigLogo.jpg';
 import "../../styles/sass/main.scss";
 import {Link} from 'react-router-dom';
 
 const WelcomeUser = () => {
 
     return (
-        <div class="container">
-            <div className="header">
-                <h3>¡Hola Usuario!</h3>
-                <img className="#" src={BigLogo} alt="Logo Quot" style={{width: 300, height: 300}}/>
+        <div class="welcome-container">
+            {/* <div className="welcome-cover col-8">
+                <img id="welcome-img" src="https://carddsgn.com/wp-content/uploads/2018/09/clevyr-business-cards-ar.gif" alt="Logo Quot"/>
+            </div> */}
+            <div className="welcome-content col-xl-4 .col-lg-5 col-sm-8">
+                <img id="welcomeuser-img" src="https://aeealberta.org/wp-content/uploads/2018/10/profile.png"></img>
+                <h3 className="title-1">¡Hola Usuario!</h3>
                 <p>¿Te gustaria hacer un pedido?</p>
-            </div>
-            <div className="link">
-                <Link to="/QuoteMaker" className="col-12 btn Bottom_cotizar">Cotiza ahora</Link>
-                <br/>
-                <Link to="/MyOrders" className="">Revisar mis pedidos</Link>
+                <Link to="/QuotMaker"><button className="btn buttom-cotizar">Cotiza ahora</button></Link>
+                <br />
+                <Link to="/MyOrders"><p className="link-1" id="revisar-pedidos">Revisar mis pedidos</p></Link>
             </div>
         </div>
     )
